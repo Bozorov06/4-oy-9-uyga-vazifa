e7af50a2381a1f7d8e3eb14b6edf64bcec8068c5
Bozorov06 <qoyilbekbozorov160@gmail.com>
2024-12-10 20:30:26 +0500

    ulandi

---
+++ main.js
@@ -0,0 +1,88 @@
+//   AT METHOD 
+//  1.Massivning oxiridan berilgan indeksga nisbatan qiymatni topuvchi funksiyani yozing. (Masalan, -1 indeksdan ikkinchi elementni toping.)
+
+// ==================================================
+// let num = [1,2,3,4,5,8,7,6,9,];
+// console.log(num.at(-2));
+
+
+// 2.Berilgan massivdagi barcha juft indeksdagi elementlarni yangi massivga yig'uvchi funksiyani yarating.
+
+// ==================================================
+// let res = [];
+// function sum(num){
+//     for (let i = 1; i < num.length; i++) {
+//        if (i%2==0) {
+//         res.push(num[i])
+//        } 
+//     }
+//     return res
+// }
+// let num = [1, 2, 3, 4, 5, 8, 7, 6, 9];
+// console.log((sum(num)));
+
+
+// 3.Massivning ixtiyoriy indeksidagi qiymatni o'zgartirib, yangi massiv qaytaruvchi funksiyani yarating.
+
+// ==================================================
+
+
+// JOIN METODI
+
+// 1.Massivning barcha elementlarini berilgan biriktiruvchi belgi bilan birlashtirib, umumiy uzunligini qaytaruvchi funksiyani yozing.
+// let num = [1,2,3,4,5,6,7]
+// let res = num.join (',')
+// console.log(res);
+// function sum(num) {
+//     for (let i = 0; i < num.length; i++) {
+//         res.push(num)
+//     }
+//     return res
+// }
+
+// console.log(num.length);
+
+
+
+// 2.Berilgan massiv elementlarini teskari tartibda qo'shuvchi va belgi bilan birlashtiruvchi funksiyani yarating.
+
+// ==================================================
+
+// 3.Berilgan massivning faqat toq sonli elementlarini ajratib, ularni biriktiruvchi funksiyani yarating.
+
+// ==================================================
+
+// toString
+
+// 1.Massiv elementlarini stringga aylantirib, har bir elementning uzunligini qaytaruvchi funksiyani yarating.
+
+// ==================================================
+// let arr =[1,2,3,4,5];
+
+
+// 2.Massiv elementlarini string shaklida birlashtirib, belgilangan elementni qidiruvchi funksiyani yozing.
+
+// ==================================================
+
+
+// 3.Massivni stringga aylantirgandan so'ng, oxiridan boshlab bir elementni olib tashlaydigan funksiyani yarating.
+
+//  CONCAT
+
+// 1.Ikki massivni birlashtirib, oxirgi elementni olib tashlaydigan funksiyani yozing.
+
+// ==================================================
+
+// let arr1 = [1,2,3,4,5,6];
+// let arr2 =[7,8,9,]
+// let res = arr1.concat(arr2)
+// console.log(res);
+
+// 2.Bir nechta massivlarni birlashtirib, faqat unikal qiymatlarni saqlaydigan funksiyani yarating.
+// chunmadim
+
+// ==================================================
+
+// 3.Bir nechta massivlarni birlashtirib, faqat musbat sonlarni o'z ichiga olgan yangi massiv qaytaruvchi funksiyani yozing.
+
+// ==================================================
